7c172e207b670123efeadc9840b4e381c27ece2f
Nikolay Kyurkchiyski <n.kyurkchiyski@gmail.com>
2022-06-10 15:05:27 +0300

    add entity to ontology on edit

--- src/main/java/com/apollo/music/views/managecontent/AbstractContentManagerView.java
+++ src/main/java/com/apollo/music/views/managecontent/AbstractContentManagerView.java
@@ -80,18 +80,16 @@ public abstract class AbstractContentManagerView<T extends EntityWithId, S exten
         {
             if (form.isSaved() && validate(form.getBean())) {
                 final T bean = form.getBean();
-                final boolean isCreate = bean.getId() == null;
                 updateEntity(bean);
 
                 form.clearForm();
                 dialog.close();
                 refreshGrid();
 
-                if (isCreate) {
-                    AgentManager.createNewAgent(bean.getClass().getSimpleName() + "Creator",
-                            getEditorAgentClassType(),
-                            new Object[]{"add", bean.createFieldValueMap()});
-                }
+                AgentManager.createNewAgent(bean.getClass().getSimpleName() + "Creator",
+                        getEditorAgentClassType(),
+                        new Object[]{"add", bean.createFieldValueMap()});
+
                 Notification.show(String.format(ViewConstants.Notification.ENTITY_SAVED, getEntityClass().getSimpleName()));
             }
         });
